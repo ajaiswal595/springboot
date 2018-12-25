43c526fbf52914bd712e59679586fe1068da47bd
JAIAJ02 <ajay.jaiswal@ca.com>
2018-12-25 20:11:01 +0530

    @Primary will be used by spring as default or primary in case of amibiguity while wiring up datasources

--- das-boot/src/main/java/config/PersistenceConfiguration.java
+++ das-boot/src/main/java/config/PersistenceConfiguration.java
@@ -6,13 +6,20 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class PersistenceConfiguration {
 	@Bean
 	@ConfigurationProperties
+	@Primary
 	public DataSource dataSource() {
 		return DataSourceBuilder.create().build();
 	}
 
+	@Bean
+	@ConfigurationProperties
+	public DataSource flywayDataSource() {
+		return DataSourceBuilder.create().build();
+	}
 }
