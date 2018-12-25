405630e26baf9a254cefbc65259be76641c8e904
JAIAJ02 <ajay.jaiswal@ca.com>
2018-12-25 20:18:29 +0530

    Added @FlywayDatasource

--- das-boot/src/main/java/config/PersistenceConfiguration.java
+++ das-boot/src/main/java/config/PersistenceConfiguration.java
@@ -2,6 +2,7 @@ package config;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,14 +12,15 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class PersistenceConfiguration {
 	@Bean
-	@ConfigurationProperties
+	@ConfigurationProperties(prefix="spring.datasource")
 	@Primary
 	public DataSource dataSource() {
 		return DataSourceBuilder.create().build();
 	}
 
 	@Bean
-	@ConfigurationProperties
+	@ConfigurationProperties(prefix="datasource.flyway")
+	@FlywayDataSource
 	public DataSource flywayDataSource() {
 		return DataSourceBuilder.create().build();
 	}
